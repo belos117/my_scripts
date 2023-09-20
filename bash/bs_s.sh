c88373cd77c9e225c43a6064520b0e0cbddbf263
belos117 <belos117@gmail.com>
2023-09-20 17:54:20 +0000

    added description, author and date variables

--- bash/bs_s.sh
+++ bash/bs_s.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #Bash Script to create an executable bash file
+#Author: Silent~Storm Henkel
+#Date: 20/09/2023
 
 echo "This is a bash script to create an executable bash file"
 
@@ -7,14 +9,18 @@ sleep 2
 
 echo "Name of file?"
 
-read name
+	read name
 
 echo "What does your script do"
 
-read description
+	read description
 
 echo "#!/bin/bash" > ./$name.sh
 
-echo "#$description" >> ./$name.sh
+echo "#Description: $description" >> ./$name.sh
 
-chmod 777 ./$name.sh
+echo "#Author: $USER" >> ./$name.sh
+
+echo "#Date: $(date)" >> ./$name.sh
+
+chmod 700 ./$name.sh
