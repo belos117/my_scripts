366481cd2caf1ed5abe3497bd0735f55f0f0dcce
belos117 <belos117@gmail.com>
2023-09-20 17:26:36 +0000

    made changes to bs_s to add description

--- bash/bs_s.sh
+++ bash/bs_s.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 #Bash Script to create an executable bash file
 
-echo "What name do you want to give to your file?"
+echo "This is a bash script to create an executable bash file"
+
+sleep 2
+
+echo "Name of file?"
 
 read name
 
+echo "What does your script do"
+
+read description
+
 echo "#!/bin/bash" > ./$name.sh
+
+echo "#$description" >> ./$name.sh
 
 chmod 777 ./$name.sh
