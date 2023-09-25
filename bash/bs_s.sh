e2b5bda5d8104632f76f84ec7a8f5cb4687eaaad
belos117 <belos117@gmail.com>
2023-09-25 04:25:21 +0000

    Added multi file support to bs_s.sh

--- bash/bs_s.sh
+++ bash/bs_s.sh
@@ -3,36 +3,64 @@
 #Author: Silent~Storm Henkel
 #Date: 20/09/2023
 
-echo "This is a bash script to create an executable bash file"
+echo "This is a bash script to create an executable file"
 
-sleep 2
+sleep 1
 
 echo "Name of file?"
-
 	read name
 
-echo "What does your script do"
-
+echo "What does your script do?"
 	read description
 
 echo "Author"
-
 	read author
 
-echo "#!/bin/bash" > ./$name.sh
+echo "Select the file type:
+1 - Bash
+2 - Python
+3 - Perl"
 
-echo "#Description: $description" >> ./$name.sh
+read type
+
+case $type in
+    1)
+      compiler="#!/bin/bash"
+      ext="sh"
+      ;;
+    2)
+      compiler="#!/bin/python"
+      ext="py"
+      ;;
+    3)
+      compiler="#!/bin/perl"
+      ext="pl"
+      ;;
+esac
+
+  if [[ $type == 1 ]]; then
+     echo $compiler > ./$name.$ext
+elif [[ $type == 2 ]]; then
+     echo $compiler > ./$name.$ext
+elif [[ $type == 3 ]]; then
+     echo $compiler > ./$name.$ext
+else
+     echo "Your selection is invalid, please select again"
+  fi
+
+echo "#Description: $description" >> ./$name.$ext
 
 echo
-	if [[ $author == "" ]]; then
+   if [[ $author == "" ]]; then
+    echo "#Author: Silent~Storm Henkel" >> ./$name.$ext
+   else
+    echo "#Author: $author" >> ./$name.$ext
+   fi
 
-		echo "#Author: Silent~Storm Henkel" >> ./$name.sh
-	else
+echo "#Date: $(date)" >> ./$name.$ext
 
-		echo"#Author: $author" >> ./$name.sh
+chmod 700 ./$name.$ext
 
-	fi
+sleep 2
 
-echo "#Date: $(date)" >> ./$name.sh
-
-chmod 700 ./$name.sh
+echo "Your file has been created successfully, happy coding!!!"
